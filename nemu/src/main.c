5789dd1154ec1953b7af310808edf96302473225
tracer-ics2019 <tracer@njuics.org>
2020-12-23 19:16:15 +0800

    >  compile U201714841 chuqiz Linux debian 4.19.0-11-amd64 #1 SMP Debian 4.19.146-1 (2020-09-17) x86_64 GNU/Linux  19:16:15 up 3 days,  7:17,  1 user,  load average: 0.55, 0.20, 0.07 475f04f7d02f124f5a2e9c86d7f6254357b882f3

--- nemu/src/main.c
+++ nemu/src/main.c
@@ -5,6 +5,9 @@ int main(int argc, char *argv[]) {
   /* Initialize the monitor. */
   int is_batch_mode = init_monitor(argc, argv);
 
+
+	/* debug */
+	is_batch_mode = 0;
   /* Receive commands from user. */
   ui_mainloop(is_batch_mode);
 
