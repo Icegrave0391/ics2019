cc65e079a1015a3e72d24fe774833f95bbbe07e1
tracer-ics2019 <tracer@njuics.org>
2020-12-23 14:30:48 +0800

    >  compile U201714841 chuqiz Linux debian 4.19.0-11-amd64 #1 SMP Debian 4.19.146-1 (2020-09-17) x86_64 GNU/Linux  14:30:47 up 3 days,  2:32,  1 user,  load average: 1.32, 0.32, 0.11 6d3e76909b8a902c94048f6669ed416c47f38f4a

--- nemu/src/isa/x86/exec/cc.c
+++ nemu/src/isa/x86/exec/cc.c
@@ -15,13 +15,34 @@ void rtl_setcc(rtlreg_t* dest, uint8_t subcode) {
   // dest <- ( cc is satisfied ? 1 : 0)
   switch (subcode & 0xe) {
     case CC_O:
-    case CC_B:
+			rtl_get_OF(dest);
+			break;
+		case CC_B:
+			rtl_get_CF(dest);
+			break;
     case CC_E:
-    case CC_BE:
+			rtl_get_ZF(dest);
+			break;
+    case CC_BE:  // CF or OF
+			rtl_get_CF(&s0);
+			rtl_get_ZF(&s1);
+			rtl_or(dest, &s0, &s1);
+			break;
     case CC_S:
-    case CC_L:
-    case CC_LE:
-      TODO();
+			rtl_get_SF(dest);
+			break;
+    case CC_L:  // SF != OF
+			rtl_get_SF(&s0);
+			rtl_get_OF(&s1);
+			rtl_xor(dest, &s0, &s1);
+			break;
+    case CC_LE:  // ZF or SF != OF
+			rtl_get_ZF(dest);
+			rtl_get_SF(&s0);
+			rtl_get_OF(&s1);
+			rtl_xor(&s0, &s0, &s1);
+			rtl_or(dest, &s0, dest);
+			break;
     default: panic("should not reach here");
     case CC_P: panic("n86 does not have PF");
   }
