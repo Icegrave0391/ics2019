4cde98992a7ada7957c0dc7598c1bd1404c652e4
tracer-ics2019 <tracer@njuics.org>
2020-12-23 19:49:09 +0800

    >  compile U201714841 chuqiz Linux debian 4.19.0-11-amd64 #1 SMP Debian 4.19.146-1 (2020-09-17) x86_64 GNU/Linux  19:49:08 up 3 days,  7:50,  1 user,  load average: 0.65, 0.41, 0.16 c5aeee8e8a643ec6f9152f74c4a78fc45ac72eb

--- nemu/src/isa/x86/exec/exec.c
+++ nemu/src/isa/x86/exec/exec.c
@@ -41,7 +41,7 @@ make_group(gp1,
 
 	/* 0xff */
 	make_group(gp5,
-			   EMPTY, EMPTY, EX(call_rm), EX(call),
+			   EX(inc), EMPTY, EX(call_rm), EX(call),
 			   EMPTY, EMPTY, EX(push), EMPTY)
 
 	/* 0x0f 0x01*/
