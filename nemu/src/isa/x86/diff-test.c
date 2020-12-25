01f61387d67efcabc4d77f1c222c166804ffe151
tracer-ics2019 <tracer@njuics.org>
2020-12-25 16:59:18 +0800

    >  compile U201714841 chuqiz Linux debian 4.19.0-11-amd64 #1 SMP Debian 4.19.146-1 (2020-09-17) x86_64 GNU/Linux  16:59:17 up 4 days, 13:27,  1 user,  load average: 2.18, 0.56, 0.25 8b21c631f3dc51e994a61578fde75c02314284dc

--- nemu/src/isa/x86/diff-test.c
+++ nemu/src/isa/x86/diff-test.c
@@ -2,7 +2,52 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  return false;
+  bool flg = true;
+	if(ref_r->eax != reg_l(R_EAX)){
+		printf("pc: 0x%x\tcpu.eax: 0x%x\tref_r.eax: 0x%x\n", cpu.pc, reg_l(R_EAX), ref_r->eax);
+		flg = false;
+	}
+	if (ref_r->ecx != reg_l(R_ECX))
+	{
+		printf("pc: 0x%x\tcpu.ecx: 0x%x\tref_r.ecx: 0x%x\n", cpu.pc, reg_l(R_ECX), ref_r->ecx);
+		flg = false;
+	}
+	if (ref_r->edx != reg_l(R_EDX))
+	{
+		printf("pc: 0x%x\tcpu.edx: 0x%x\tref_r.edx: 0x%x\n", cpu.pc, reg_l(R_EDX), ref_r->edx);
+		flg = false;
+	}
+	if (ref_r->ebx != reg_l(R_EBX))
+	{
+		printf("pc: 0x%x\tcpu.ebx: 0x%x\tref_r.ebx: 0x%x\n", cpu.pc, reg_l(R_EBX), ref_r->ebx);
+		flg = false;
+	}
+	if (ref_r->esp != reg_l(R_ESP))
+	{
+		printf("pc: 0x%x\tcpu.esp: 0x%x\tref_r.esp: 0x%x\n", cpu.pc, reg_l(R_ESP), ref_r->esp);
+		flg = false;
+	}
+	if (ref_r->ebp != reg_l(R_EBP))
+	{
+		printf("pc: 0x%x\tcpu.ebp: 0x%x\tref_r.ebp: 0x%x\n", cpu.pc, reg_l(R_EBP), ref_r->ebp);
+		flg = false;
+	}
+	if (ref_r->esi != reg_l(R_ESI))
+	{
+		printf("pc: 0x%x\tcpu.esi: 0x%x\tref_r.esi: 0x%x\n", cpu.pc, reg_l(R_ESI), ref_r->esi);
+		flg = false;
+	}
+	if (ref_r->edi != reg_l(R_EDI))
+	{
+		printf("pc: 0x%x\tcpu.edi: 0x%x\tref_r.edi: 0x%x\n", cpu.pc, reg_l(R_EDI), ref_r->edi);
+		flg = false;
+	}
+	if (ref_r->pc != cpu.pc)
+	{
+		printf("cpu.pc: 0x%x\tref.pc: 0x%x\n", cpu.pc, ref_r->pc);
+		flg = false;
+	}
+	return flg;
 }
 
 void isa_difftest_attach(void) {
