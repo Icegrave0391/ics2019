314ce3487423bd492d1286990ac90f3d8eff63e0
tracer-ics2019 <tracer@njuics.org>
2020-12-25 12:12:53 +0800

    >  run U201714841 chuqiz Linux debian 4.19.0-11-amd64 #1 SMP Debian 4.19.146-1 (2020-09-17) x86_64 GNU/Linux  12:12:52 up 4 days, 12:06,  1 user,  load average: 0.08, 0.02, 0.01 7bb88e281ad9e37b72a29c96129fccbffdb74194

--- nexus-am/tests/cputest/tests/bubble-sort.c
+++ nexus-am/tests/cputest/tests/bubble-sort.c
@@ -8,6 +8,7 @@ void bubble_sort() {
 	int i, j, t;
 	for(j = 0; j < N; j ++) {
 		for(i = 0; i < N - 1 - j; i ++) {
+			printf("a[%d]: %d and a[%d]: %d\n",i,a[i],i+1,a[i+1]);
 			if(a[i] > a[i + 1]) {
 				t = a[i];
 				a[i] = a[i + 1];
