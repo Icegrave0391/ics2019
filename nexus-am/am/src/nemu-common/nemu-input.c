7f961aab20a96ed7256c5c80e6fb9b960471ae48
tracer-ics2019 <tracer@njuics.org>
2020-12-28 15:09:23 +0800

    >  run U201714841 chuqiz Linux debian 4.19.0-11-amd64 #1 SMP Debian 4.19.146-1 (2020-09-17) x86_64 GNU/Linux  15:09:22 up 5 days,  1:22,  1 user,  load average: 2.81, 0.66, 0.22 e35badbc6c33be3f1c236ed94602e42c6c1cac40

--- nexus-am/am/src/nemu-common/nemu-input.c
+++ nexus-am/am/src/nemu-common/nemu-input.c
@@ -8,8 +8,14 @@ size_t __am_input_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_INPUT_KBD: {
       _DEV_INPUT_KBD_t *kbd = (_DEV_INPUT_KBD_t *)buf;
-      kbd->keydown = 0;
-      kbd->keycode = _KEY_NONE;
+			uint32_t code = inl(KBD_ADDR);
+			if(code & 0x8000){   // code represents 'make code'
+				kbd->keydown = 1;
+			}
+			else{                // code represents 'break code'
+				kbd->keydown = 0;
+			}
+      kbd->keycode = code;
       return sizeof(_DEV_INPUT_KBD_t);
     }
   }
