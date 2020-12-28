824e9997661c92287c5dab10ad65520aec1ea84b
tracer-ics2019 <tracer@njuics.org>
2020-12-28 17:37:50 +0800

    >  compile U201714841 chuqiz Linux debian 4.19.0-11-amd64 #1 SMP Debian 4.19.146-1 (2020-09-17) x86_64 GNU/Linux  17:37:49 up 5 days,  3:28,  1 user,  load average: 1.27, 0.31, 0.10 bf6c97f2813a4884c736cc7c1de336924c6c4371

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -2,12 +2,14 @@
 #include <amdev.h>
 #include <nemu.h>
 
+#define SCREEN_H 300
+#define SCREEN_W 400
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_INFO: {
       _DEV_VIDEO_INFO_t *info = (_DEV_VIDEO_INFO_t *)buf;
-      info->width = 0;
-      info->height = 0;
+      info->width = SCREEN_W;
+      info->height = SCREEN_H;
       return sizeof(_DEV_VIDEO_INFO_t);
     }
   }
@@ -18,7 +20,16 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
       _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-
+			/* write to video memory MMIO from 0xa0000000, MODE(400x300x32) */
+			uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+			uint32_t *pixels = ctl->pixels; // pixels (0x00RRGGBB)
+			int x_off = ctl->x, y = ctl->y;
+			for(int i = 0; i < ctl->h; i++){
+				for(int j = 0; j < ctl->w; j++){
+					if((y + i) > (SCREEN_H - 1) || (x_off + j) > (SCREEN_W - 1))break;
+					fb[(y+i)*SCREEN_W + x_off + j] = pixels[i * ctl->w + j];
+				}
+			}
       if (ctl->sync) {
         outl(SYNC_ADDR, 0);
       }
@@ -29,4 +40,11 @@ size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
 }
 
 void __am_vga_init() {
+	int i;
+	int size = screen_width() * screen_height();
+	uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+	for(i = 0; i < size; i++){
+		fb[i] = i;
+	}
+	draw_sync();
 }
