fb71f8ba06f17f54042f491fa05c7c7bf60e9dca
tracer-ics2019 <tracer@njuics.org>
2021-01-02 16:51:03 +0800

    >  compile U201714841 chuqiz Linux debian 4.19.0-11-amd64 #1 SMP Debian 4.19.146-1 (2020-09-17) x86_64 GNU/Linux  16:51:03 up 6 days, 13 min,  1 user,  load average: 0.94, 0.52, 0.32 949e779a4c79a9c9268e13bbad479eac50754085

--- nemu/src/isa/x86/exec/system.c
+++ nemu/src/isa/x86/exec/system.c
@@ -41,9 +41,13 @@ make_EHelper(int) {
 }
 
 make_EHelper(iret) {
-  TODO();
+//   TODO();
+	rtl_pop(&decinfo.jmp_pc);
+	rtl_pop(&cpu.cs);
+	rtl_pop(&cpu.eflags.eflags_32);
 
-  print_asm("iret");
+	rtl_j(decinfo.jmp_pc);
+	print_asm("iret");
 }
 
 uint32_t pio_read_l(ioaddr_t);
