20f3dd32565a1eff6108eb217179e66e8775df9e
tracer-ics2019 <tracer@njuics.org>
2021-01-04 15:50:57 +0800

    >  compile U201714841 chuqiz Linux debian 4.19.0-11-amd64 #1 SMP Debian 4.19.146-1 (2020-09-17) x86_64 GNU/Linux  15:50:57 up 7 days,  4:10,  1 user,  load average: 0.55, 0.24, 0.19 fdcd787cf53d069212b1a1487c8fc634fd2c5738

--- nanos-lite/src/syscall.c
+++ nanos-lite/src/syscall.c
@@ -2,7 +2,8 @@
 #include "syscall.h"
 
 uint32_t sys_yield();
-uint32_t sys_exit();
+uint32_t sys_exit(int state);
+uint32_t sys_write(int fd, const void *buf, size_t count);
 
 _Context* do_syscall(_Context *c) {
   uintptr_t a[4];
@@ -15,7 +16,8 @@ _Context* do_syscall(_Context *c) {
 	uintptr_t res = 0;
   switch (a[0]) {  /* syscall.type (called by user at _syscall_(type, args)) */
 		case SYS_yield: res = sys_yield();break;
-		case SYS_exit: sys_exit(a[1]);break;
+		case SYS_exit: res = sys_exit(a[1]);break;
+		case SYS_write: res = sys_write(a[1], (void *)a[2], a[3]);
     default: panic("Unhandled syscall ID = %d", a[0]);
   }
 
@@ -31,4 +33,16 @@ uint32_t sys_yield(){
 
 uint32_t sys_exit(int state){
 	_halt(state);
+}
+uint32_t sys_write(int fd, const void *buf, size_t count){
+	/* only write to stdout or stderr,
+	 * for didn't implement file system yet.
+	 */
+	if(fd == 1 || fd == 2){
+		size_t i;
+		for(i = 0; i < count; i++){
+			_putc(((char *)buf)[i]);
+		}
+	}
+	return count;
 }
