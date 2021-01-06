5223469ac6ce1597446b2a2a26fab45ba716b386
tracer-ics2019 <tracer@njuics.org>
2021-01-07 02:23:22 +0800

    >  run U201714841 chuqiz Linux debian 4.19.0-11-amd64 #1 SMP Debian 4.19.146-1 (2020-09-17) x86_64 GNU/Linux  02:23:22 up 8 days,  6:04,  1 user,  load average: 1.99, 1.03, 0.90 6f8cda4c7f9d28a6669cc71016789fce29ab6e5

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -90,21 +90,25 @@ size_t fs_read(int fd, void *buf, size_t len){
 	/* operate reading via ramdisk APIs */
 	Finfo file = file_table[fd];
 	size_t rlen = -1;
+
+	/* set the correct read size */
+	if (file.open_offset > file.size)
+	{
+		printf("[fs_read failed] file %d current open offset %lu > file size %lu.\n", fd, file.open_offset, file.size);
+		return 0; //EOF
+	}
+
 	if (file.read){
 		rlen = file.read(buf, file.open_offset, len);
 		(&file_table[fd])->open_offset += rlen;
 	}
 	else{
 		/* set the correct read size */
-		if(file.open_offset > file.size){
-			printf("[fs_read failed] file %d current open offset %lu> file size %lu.\n", fd, file.open_offset, file.size);
-			return -1;
-		}
 		/* set correct base offset */
 		size_t base_offset = file.open_offset < 0 ? 0 : file.open_offset;
 		len = file.open_offset < 0 ? len + file.open_offset : len;
 		if (len < 0){
-			return -1;
+			return 0;
 		}
 		if(base_offset + len > file.size){
 			len = file.size - base_offset;
@@ -122,20 +126,20 @@ size_t fs_write(int fd, const void *buf, size_t len)
 	assert(fd >= 0 && fd < NR_FILES);
 	size_t wlen = -1;
 	Finfo file = file_table[fd];
+	if (file.open_offset > file.size){
+		printf("[fs_write failed] file %d current open offset %lu> file size %lu.\n", fd, file.open_offset, file.size);
+		return 0;
+	}
 	if(file.write){
 		wlen = file.write(buf, file.open_offset, len);
 		(&file_table[fd])->open_offset += wlen;
 	}
 	else{
-		if(file.open_offset > file.size){
-			printf("[fs_write failed] file %d current open offset %lu> file size %lu.\n", fd, file.open_offset, file.size);
-			return -1;
-		}
 		/* set correct base offset */
 		size_t base_offset = file.open_offset < 0 ? 0 : file.open_offset;
 		len = file.open_offset < 0 ? len + file.open_offset : len;
 		if(len < 0){
-			return -1;
+			return 0;
 		}
 		if(base_offset + len > file.size){
 			len = file.size - base_offset;
