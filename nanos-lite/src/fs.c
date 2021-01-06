fed8ce1c798a531f08699325a04d6ac2cfd851fd
tracer-ics2019 <tracer@njuics.org>
2021-01-07 02:05:40 +0800

    >  run U201714841 chuqiz Linux debian 4.19.0-11-amd64 #1 SMP Debian 4.19.146-1 (2020-09-17) x86_64 GNU/Linux  02:05:38 up 8 days,  5:47,  1 user,  load average: 1.38, 1.14, 1.04 a04e95c95cbedd0fa64094656b2ea94579f637

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -52,7 +52,7 @@ static Finfo file_table[] __attribute__((used)) = {
 	{"/dev/events", 0, 0, 0, events_read, invalid_write},
   {"/dev/fb", 0, 0, 0, invalid_read, fb_write},
   {"/dev/fbsync", 0, 0, 0, invalid_read, fbsync_write},
-  {"/proc/dispinfo", 0, 0, 0, dispinfo_read, invalid_write},
+  {"/proc/dispinfo", 128, 0, 0, dispinfo_read, invalid_write}, /* important to set size <- 128, to ensure fgetc() in ndl.c works */
 #include "files.h"
 };
 
