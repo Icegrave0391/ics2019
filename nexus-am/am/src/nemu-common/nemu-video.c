e0a5f6b848e9ac24268c6b748466a9288ef8f78e
tracer-ics2019 <tracer@njuics.org>
2021-01-07 00:50:21 +0800

    >  run U201714841 chuqiz Linux debian 4.19.0-11-amd64 #1 SMP Debian 4.19.146-1 (2020-09-17) x86_64 GNU/Linux  00:50:20 up 8 days,  4:31,  1 user,  load average: 2.06, 1.49, 1.06 b53b3bb9c247b23d8be531004aa25313a04da2b8

--- nexus-am/am/src/nemu-common/nemu-video.c
+++ nexus-am/am/src/nemu-common/nemu-video.c
@@ -1,7 +1,7 @@
 #include <am.h>
 #include <amdev.h>
 #include <nemu.h>
-
+#include <klib.h>
 #define SCREEN_H 300
 #define SCREEN_W 400
 size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
@@ -19,22 +19,42 @@ size_t __am_video_read(uintptr_t reg, void *buf, size_t size) {
 size_t __am_video_write(uintptr_t reg, void *buf, size_t size) {
   switch (reg) {
     case _DEVREG_VIDEO_FBCTL: {
-      _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
-			/* write to video memory MMIO from 0xa0000000, MODE(400x300x32) */
+      // _DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
+			// /* write to video memory MMIO from 0xa0000000, MODE(400x300x32) */
+			// uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+			// uint32_t *pixels = ctl->pixels; // pixels (0x00RRGGBB)
+			// int x_off = ctl->x, y = ctl->y;
+			// for(int i = 0; i < ctl->h; i++){
+			// 	for(int j = 0; j < ctl->w; j++){
+			// 		if((y + i) > (SCREEN_H - 1) || (x_off + j) > (SCREEN_W - 1))break;
+			// 		fb[(y+i)*SCREEN_W + x_off + j] = pixels[i * ctl->w + j];
+			// 	}
+			// }
+      // if (ctl->sync) {
+      //   outl(SYNC_ADDR, 0);
+      // }
+      // return size;
+			_DEV_VIDEO_FBCTL_t *ctl = (_DEV_VIDEO_FBCTL_t *)buf;
+			int x = ctl->x, y = ctl->y, w = ctl->w, h = ctl->h;
+
+			/* fb: the whole data that will be painted on the screen,
+			 * but we only need to fill part of data (that is from (x, y) to (x + w, y + h)) by copy from pixels
+			 */
 			uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
-			uint32_t *pixels = ctl->pixels; // pixels (0x00RRGGBB)
-			int x_off = ctl->x, y = ctl->y;
-			for(int i = 0; i < ctl->h; i++){
-				for(int j = 0; j < ctl->w; j++){
-					if((y + i) > (SCREEN_H - 1) || (x_off + j) > (SCREEN_W - 1))break;
-					fb[(y+i)*SCREEN_W + x_off + j] = pixels[i * ctl->w + j];
-				}
+
+			/* pixels: the data that will be painted. we show copy the data in pixels to fb. */
+			uint32_t *pixels = ctl->pixels;
+			int copy_size = sizeof(uint32_t) * (SCREEN_W - x < w ? SCREEN_W - x : w);
+			for (int i = 0; i < h && y + i < SCREEN_H; ++i)
+			{
+				memcpy(&fb[(i + y) * SCREEN_W + x], pixels + i * w, copy_size);
 			}
-      if (ctl->sync) {
-        outl(SYNC_ADDR, 0);
-      }
-      return size;
-    }
+			if (ctl->sync)
+			{
+				outl(SYNC_ADDR, 0);
+			}
+			return size;
+		}
   }
   return 0;
 }
